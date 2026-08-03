8dfe85bc4df70c2b9020450c7a0661aa22ad5f55
Fawaad <fkhan@edata.ae>
2026-08-03 13:19:34 +0400

    Trigger CI on develop pull requests

---
+++ VinAgentAPI/Controllers/VinController.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace VinAgentAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class VinController : ControllerBase
+    {
+
+        private readonly ILogger<VinController> _logger;
+
+        public VinController(ILogger<VinController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetVin")]
+        public string Get()
+        {
+            return "This is VIN";
+        }
+    }
+}
