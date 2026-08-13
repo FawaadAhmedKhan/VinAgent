d96b8086b6227e8be3c4d73f3a92e2ae5431f8cd
Fawaad <fkhan@edata.ae>
2026-08-13 14:24:38 +0400

    value is 4200

--- VinAgentAPI/Controllers/VinController.cs
+++ VinAgentAPI/Controllers/VinController.cs
@@ -28,7 +28,7 @@ namespace VinAgentAPI.Controllers
         [HttpGet("Valuation")]
         public string Valuation()
         {
-            return "Valuation is: aed 4100";
+            return "Valuation is: aed 4200";
         }
     }
 }
